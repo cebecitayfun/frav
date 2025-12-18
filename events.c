b5e6b7697dc905f2f614f76900b217364e32c5ac
Tayfun Cebeci <tayfuncbc602@gmail.com>
2025-12-18 23:29:23 +0300

    44

--- events.c
+++ events.c
@@ -4,6 +4,8 @@ int	close_handler(t_fractol *fractol)
 {
 	mlx_destroy_image(fractol->mlx_connection, fractol->img.img_ptr);
 	mlx_destroy_window(fractol->mlx_connection, fractol->mlx_window);
+	mlx_destroy_display(fractol->mlx_connection);
+	free(fractol->mlx_connection);
 	exit(0);
 	return (0);
 }
